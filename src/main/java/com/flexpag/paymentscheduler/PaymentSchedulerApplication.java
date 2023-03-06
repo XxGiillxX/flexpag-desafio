05700a41bd58bb65eebab77c5e97ad3dc605bfa9
GilbertoMacena <gilbertomacena@gmail.com>
2023-03-06 00:38:16 -0300

    Commit payment scheduler
    Commit with payment scheduler project.

---
+++ src/main/java/com/flexpag/paymentscheduler/PaymentSchedulerApplication.java
@@ -0,0 +1,15 @@
+package com.flexpag.paymentscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class PaymentSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSchedulerApplication.class, args);
+	}
+
+}
